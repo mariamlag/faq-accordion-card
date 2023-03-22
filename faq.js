2b9badf98585a992d6109ca8d8ab7fad5800111b
mariamlag <114602082+mariamlag@users.noreply.github.com>
2023-03-22 22:14:19 +0400

    s

--- faq.js
+++ faq.js
@@ -2,42 +2,42 @@
 let team_up = document.querySelectorAll('.icon_down');
 const all_question = document.querySelectorAll('.all');
 
-
-
- let active = null;
+let active = null;
 
 all_question.forEach((all) => 
     all.addEventListener('click', (event) => {
    
-    if(active){
-      
-        active.nextElementSibling.classList.toggle('active');
-        active.classList.remove('bold');
+        if (active) {
+            if (active === all) {
+                // Clicked element is the same as the active element.
+                active.nextElementSibling.classList.remove('active');
+                active.classList.remove('bold');
+                team_up.forEach((icon) => {
+                    if (icon.closest('.all') === active) {
+                        icon.classList.remove('icon_up');
+                    }
+                });
+                active = null;
+                return;
+            }
+            // Clicked element is different from the active element.
+            active.nextElementSibling.classList.remove('active');
+            active.classList.remove('bold');
+        }
+    
+        all.nextElementSibling.classList.add('active');
+        event.target.classList.add('bold');
 
         team_up.forEach((icon) => {
-            if(icon.closest('.all') !== active) {
+            if (icon.closest('.all') === all) {
+                icon.classList.add('icon_up');
+            } else {
                 icon.classList.remove('icon_up');
             }
         });
-        
-    }
-    
-    all.nextElementSibling.classList.toggle('active');
-    event.target.classList.add('bold');
 
-    team_up.forEach((icon) => {
-        if(icon.closest('.all') === all) {
-            icon.classList.toggle('icon_up');
-        } else {
-            icon.classList.remove('icon_up');
-        }
-    });
-
-    
-
-    active = all;
+        active = all;
     })
 );
-
 
 
